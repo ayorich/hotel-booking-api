0b88cbd12070845d59ccc2b74fd08dd1ca3492bf
ayorich <kayodele17@yahoo.com>
2020-09-07 10:01:28 +0100

    post request added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -23,6 +24,26 @@ app.get('/api/v1/hotels', (req, res) => {
     },
   });
 });
+
+app.post('/api/v1/hotels', (req, res) => {
+  // console.log(req.body);
+  const newId = hotels[hotels.length - 1].id + 1;
+  const newHotel = Object.assign({ id: newId }, req.body);
+  hotels.push(newHotel);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/hotel-simple.json`,
+    JSON.stringify(hotels),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          hotel: newHotel,
+        },
+      });
+    }
+  );
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`app runing on port ${port}....`);
