bc6dad687564ce4dab096b7403ef1bd58a927587
ayorich <kayodele17@yahoo.com>
2020-09-07 08:20:09 +0100

    get -post request

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello fro the server side');
+  res
+    .status(200)
+    .json({ message: 'Hello fro the server side', app: 'booking app' });
+});
+app.post('/', (req, res) => {
+  res.status(200).json({ message: 'you can post here', app: 'booking app' });
 });
 const port = 3000;
 app.listen(port, () => {
