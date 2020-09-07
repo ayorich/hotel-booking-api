bdd7c94c2f553d6ad77475733383b573938f2f5f
ayorich <kayodele17@yahoo.com>
2020-09-07 11:56:48 +0100

    passing url params

--- app.js
+++ app.js
@@ -25,6 +25,25 @@ app.get('/api/v1/hotels', (req, res) => {
   });
 });
 
+app.get('/api/v1/hotels/:id/', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const hotel = hotels.find((el) => el.id === id);
+  // if (id > hotels.length) {
+  if (!hotel) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      hotel,
+    },
+  });
+});
+
 app.post('/api/v1/hotels', (req, res) => {
   // console.log(req.body);
   const newId = hotels[hotels.length - 1].id + 1;
