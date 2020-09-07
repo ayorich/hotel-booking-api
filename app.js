ffe6807e104d2f2b9902ee6dfe2c42f6f528a6f8
ayorich <kayodele17@yahoo.com>
2020-09-07 09:28:14 +0100

    get request added

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello fro the server side', app: 'booking app' });
-});
-app.post('/', (req, res) => {
-  res.status(200).json({ message: 'you can post here', app: 'booking app' });
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello fro the server side', app: 'booking app' });
+// });
+// app.post('/', (req, res) => {
+//   res.status(200).json({ message: 'you can post here', app: 'booking app' });
+// });
+const hotels = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/hotel-simple.json`)
+);
+app.get('/api/v1/hotels', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: hotels.length,
+    data: {
+      hotels,
+    },
+  });
 });
 const port = 3000;
 app.listen(port, () => {
