c874c2fa0d654b306ac0d7d9dcd67ed2a61a4cb6
ayorich <kayodele17@yahoo.com>
2020-09-08 14:03:27 +0100

    connecting mongodb using moongooseconsole.log(con.connections);

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then((con) => {
+//     console.log(con.connections);
+//     console.log('DB connections successful');
+//   });
+(async () => {
+  const con = await mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+  console.log(con.connections);
+  console.log('DB connections successful');
+})();
 
 const port = process.env.PORT || 3000;
 
