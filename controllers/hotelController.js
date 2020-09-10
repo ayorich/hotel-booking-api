dc3217ab92d3e10da5f542838463bb28992a33d0
ayorich <kayodele17@yahoo.com>
2020-09-10 08:47:02 +0100

    update doc to database

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -55,13 +55,25 @@ exports.createHotel = async (req, res) => {
   }
 };
 
-exports.updateHotel = (req, res) => {
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      hotel: '<updated hotel here.../>',
-    },
-  });
+exports.updateHotel = async (req, res) => {
+  try {
+    const hotel = await Hotel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        hotel,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.deleteHotel = (req, res) => {
