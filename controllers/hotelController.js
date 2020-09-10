b680db3da4d3b8bad4446f3b63e4cd48fac1556e
ayorich <kayodele17@yahoo.com>
2020-09-10 14:09:26 +0100

    add pagination logic to getallhotel endpoint

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -4,18 +4,15 @@ const Hotel = require('../models/hotelModel');
 
 exports.getAllHotels = async (req, res) => {
   try {
-    console.log(req.query);
     // BUILD QUERY
     // 1a. FILTERING
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
-    console.log(queryObj);
 
     // 1b.ADVANCED FILTERING
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(queryStr);
 
     let query = Hotel.find(JSON.parse(queryStr));
 
@@ -30,15 +27,28 @@ exports.getAllHotels = async (req, res) => {
     // 3.LIMITING FIELD QUERY
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
-
       query = query.select(fields);
     } else {
       query = query.select('-__v');
     }
 
+    // PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numHotel = await Hotel.countDocuments();
+      if (skip >= numHotel) throw new Error('This page does not exist');
+    }
+
     // EXECUTE QUERY
+    // query.sort().select().skip().limit()
     const hotels = await query;
 
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: hotels.length,
@@ -49,10 +59,11 @@ exports.getAllHotels = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err.message,
     });
   }
 };
+
 exports.getHotel = async (req, res) => {
   try {
     const hotel = await Hotel.findById(req.params.id);
