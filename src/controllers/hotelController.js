9e343a65efbba667b512816c5f4590e02fabde88
ayorich <kayodele17@yahoo.com>
2020-09-17 05:42:52 +0100

    error-catchAsync-and-404

--- src/controllers/hotelController.js
+++ src/controllers/hotelController.js
@@ -1,5 +1,7 @@
 const Hotel = require('../models/hotelModel');
 const apiFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 
 // const hotels = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/hotel-simple.json`));
 
@@ -11,176 +13,144 @@ exports.alaisTopHotels = (req, res, next) => {
   next();
 };
 
-exports.getAllHotels = async (req, res) => {
-  try {
-    // BUILD QUERY
-    const hotelData = Hotel.find();
-    // 1a. FILTERING
-    let query = apiFeatures.filter(hotelData, req.query);
+exports.getAllHotels = catchAsync(async (req, res, next) => {
+  // BUILD QUERY
+  const hotelData = Hotel.find();
+  // 1a. FILTERING
+  let query = apiFeatures.filter(hotelData, req.query);
 
-    // 2.SORTING
-    query = apiFeatures.sort(hotelData, req.query);
+  // 2.SORTING
+  query = apiFeatures.sort(hotelData, req.query);
 
-    // 3.LIMITING FIELD QUERY
-    query = apiFeatures.limitFields(hotelData, req.query);
+  // 3.LIMITING FIELD QUERY
+  query = apiFeatures.limitFields(hotelData, req.query);
 
-    // 4. PAGINATION
-    /**
+  // 4. PAGINATION
+  /**
      * use for pagination when specified.
      * @param {hotelData} hotelData first Arg - query returned on Find().
      * @param {req.query} req.query Second Arg - request query.
      * @param {Hotel} Hotel Third Arg- hotel model.
      * @returns {query} paginated query.
      */
-    query = apiFeatures.pagination(hotelData, req.query, Hotel);
+  query = apiFeatures.pagination(hotelData, req.query, Hotel);
 
-    // EXECUTE QUERY
-    // query.sort().select().skip().limit()
-    const hotels = await query;
+  // EXECUTE QUERY
+  // query.sort().select().skip().limit()
+  const hotels = await query;
 
-    // SEND RESPONSE
-    res.status(200).json({
-      status: 'success',
-      results: hotels.length,
-      data: {
-        hotels,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: hotels.length,
+    data: {
+      hotels,
+    },
+  });
+});
+
+exports.getHotel = catchAsync(async (req, res, next) => {
+  const hotel = await Hotel.findById(req.params.id);
+  if (!hotel) {
+    return next(new AppError('No hotel found with that ID', 404));
   }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      hotel,
+    },
+  });
+});
 
-exports.getHotel = async (req, res) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
+exports.createHotel = catchAsync(async (req, res, next) => {
+  const newHotel = await Hotel.create(req.body);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        hotel,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
+  res.status(201).json({
+    status: 'success',
+    data: {
+      hotel: newHotel,
+    },
+  });
+});
+
+exports.updateHotel = catchAsync(async (req, res, next) => {
+  const reqBody = Object.keys(req.body).length;
+
+  if (reqBody === 0) throw new Error('update body is empty');
+  // eslint-disable-next-line no-extra-boolean-cast
+  const hotel = await Hotel.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!hotel) {
+    return next(new AppError('No hotel found with that ID', 404));
   }
-};
-exports.createHotel = async (req, res) => {
-  try {
-    const newHotel = await Hotel.create(req.body);
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        hotel: newHotel,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      hotel,
+    },
+  });
+});
+
+exports.deleteHotel = catchAsync(async (req, res, next) => {
+  const hotel = await Hotel.findByIdAndDelete(req.params.id);
+  if (!hotel) {
+    return next(new AppError('No hotel found with that ID', 404));
   }
-};
 
-exports.updateHotel = async (req, res) => {
-  try {
-    const reqBody = Object.keys(req.body).length;
+  res.status(204).json({
+    status: 'Success',
+    data: null,
+  });
+});
 
-    if (reqBody === 0) throw new Error('update body is empty');
-    // eslint-disable-next-line no-extra-boolean-cast
-    const hotel = await Hotel.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.getHotelStats = catchAsync(async (req, res, next) => {
+  const stats = await Hotel.aggregate([
+    {
+      $match: {
+        ratingsAverage: { $gte: 1 },
+      },
+    },
 
-    res.status(200).json({
-      status: 'Success',
-      data: {
-        hotel,
+    {
+      $group: {
+        // _id: { $toUpper: '$hotelType' },
+        _id: { $trim: { input: { $toUpper: '$hotelType' } } },
+        numHotel: { $sum: 1 },
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' },
       },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
-
-exports.deleteHotel = async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: 'Success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
-
-exports.getHotelStats = async (req, res) => {
-  try {
-    const stats = await Hotel.aggregate([
-      {
-        $match: {
-          ratingsAverage: { $gte: 1 },
-        },
+    },
+    {
+      $addFields: {
+        hotelType: '$_id',
       },
-
-      {
-        $group: {
-          // _id: { $toUpper: '$hotelType' },
-          _id: { $trim: { input: { $toUpper: '$hotelType' } } },
-          numHotel: { $sum: 1 },
-          numRatings: { $sum: '$ratingsQuantity' },
-          avgRating: { $avg: '$ratingsAverage' },
-          avgPrice: { $avg: '$price' },
-          minPrice: { $min: '$price' },
-          maxPrice: { $max: '$price' },
-        },
+    },
+    {
+      $project: {
+        _id: 0,
       },
-      {
-        $addFields: {
-          hotelType: '$_id',
-        },
+    },
+    {
+      $sort: {
+        avgPrice: -1,
       },
-      {
-        $project: {
-          _id: 0,
-        },
-      },
-      {
-        $sort: {
-          avgPrice: -1,
-        },
-      },
-      // {
-      //   $match: {
-      //     _id: { $ne: 'HOTEL' },
-      //   },
-      // },
-    ]);
-    res.status(200).json({
-      status: 'Success',
-      data: {
-        stats,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-};
+    },
+    // {
+    //   $match: {
+    //     _id: { $ne: 'HOTEL' },
+    //   },
+    // },
+  ]);
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      stats,
+    },
+  });
+});
