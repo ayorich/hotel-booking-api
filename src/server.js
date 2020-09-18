005ced86075199472e7cabff321fb248dc0d5965
ayorich <kayodele17@yahoo.com>
2020-09-18 05:34:34 +0100

    unhandled and uncaught exception

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -27,6 +33,14 @@ mongoose.connect(DB, {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app runing on port ${port}....`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION!  Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
