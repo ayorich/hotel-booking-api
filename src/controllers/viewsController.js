d3529490fc0945162bbfe7709e15f1f3e9ecd3b4
ayorich <kayodele17@yahoo.com>
2020-10-01 11:46:20 +0100

    building hotel detail page

--- src/controllers/viewsController.js
+++ src/controllers/viewsController.js
@@ -14,8 +14,15 @@ exports.getOverview = catchAsync(async (req, res) => {
     });
 });
 
-exports.getHotel = (req, res) => {
-    res.status(200).render('hotel', {
-        title: 'Hotel de Smart'
+exports.getHotel = catchAsync(async (req, res) => {
+    // 1. get data from collection
+    const hotel = await Hotel.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user'
     });
-};
+    // render template
+    res.status(200).render('hotel', {
+        title: 'Hotel de Smart',
+        hotel
+    });
+});
--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get('/', viewsController.getOverview);
 
-router.get('/hotel', viewsController.getHotel);
+router.get('/hotel/:slug', viewsController.getHotel);
 
 module.exports = router;
