8fc56f2749a395abf46f0fa3dfd8a539f2dc97ed
ayorich <kayodele17@yahoo.com>
2020-11-02 13:19:48 +0100

    email-setup-refactored

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,26 +1,56 @@
+/* eslint-disable */
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // 1. create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Ayodele Kayode <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // sendgrid
+      return 1;
     }
-    
-  });
-  // 2.define the email options
-  const mailOptions = {
-    from: 'Emma Star <hello@emma.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
-  // 3.actually send the email
-  await transporter.sendMail(mailOptions);
-};
+    // 1. create a transporter
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
 
-module.exports = sendEmail;
+    });
+  }
+
+  async send(template, subject) {
+    // send the actual email
+    // 1. RENDER HTML FOR EMAIL BASE ON PUG TEMPLATE
+    const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    });
+    // 2.define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+
+    };
+
+    // 3.CREATE A TRANSPORT AND SEND EMAIL
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to hotel booking api app!');
+  }
+};
